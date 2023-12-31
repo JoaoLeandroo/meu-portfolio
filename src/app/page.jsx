c0b260ba29fe83603213c6bac16bc0daa61df3d8
joaoleandro <new.joaoleandro@gmail.com>
2023-12-31 15:53:19 -0300

    feat: menu skills concluido(versao desktop)

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,13 @@
 import Image from "next/image"
 import Header from "./components/Header/page"
-import { IoLogoJavascript } from "react-icons/io5";
+import {  IoLogoJavascript, 
+          IoLogoHtml5, 
+          IoLogoCss3,
+          IoLogoPython,
+          IoLogoFigma
+} from "react-icons/io5"
+import {  TbBrandTailwind, TbBrandNextjs } from "react-icons/tb"
+import { FaReact, FaGitSquare, FaGithubSquare } from 'react-icons/fa'
 
 export default function Home() {
   return(
@@ -35,8 +42,19 @@ export default function Home() {
 
         <section className="max-w-[1464px] w-[80%] m-auto mt-28 text-center mb-72">
           <h2 className="text-shadow text-white font-bold text-3xl mb-5 uppercase">Minhas Skills</h2>
-          <div className="w-full h-14 bg-secundary-purple rounded-lg">
-            <IoLogoJavascript size={50} color="yellow"/>
+          <div className="w-full h-24 bg-secundary-purple rounded-lg">
+            <div className="w-full h-full flex items-center gap-11 justify-center">
+              <IoLogoPython size={60} color="#2ECCFA" title="Python" className="icons-animation"/>
+              <IoLogoFigma size={60} color="#FE2EF7" title="Figma" className="icons-animation"/>
+              <IoLogoHtml5 size={60} color="#FF8000" title="HTML5" className="icons-animation"/>
+              <IoLogoCss3 size={60} color="#0101DF" title="CSS3" className="icons-animation"/>
+              <TbBrandTailwind size={60} color="#00FFFF" title="Tailwind" className="icons-animation"/>
+              <IoLogoJavascript size={60} color="#FFFF00" title="JavaScript" className="icons-animation"/>
+              <FaReact size={60} color="#00FFFF" title="React" className="icons-animation"/>
+              <TbBrandNextjs size={60} color="#FFFFFF" title="NextJS" className="icons-animation"/>
+              <FaGitSquare size={60} color="#FF8000" title="Git" className="icons-animation"/>
+              <FaGithubSquare size={60} color="#000000" title="GitHub" className="icons-animation"/>
+          </div>
           </div>
         </section>
       </main>
