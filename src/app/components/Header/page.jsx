4ebd7c9f89c91a25d8f3bd28ba59a5e1730ea800
joaoleandro <new.joaoleandro@gmail.com>
2024-01-01 19:29:00 -0300

    fix: correção de bug

--- src/app/components/Header/page.jsx
+++ src/app/components/Header/page.jsx
@@ -1,22 +1,10 @@
-"use client"
-
 import Link from "next/link"
 import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 export default function Header() {
 
-        window.addEventListener('scroll', function() {
-            let posicaoScroll =  document.documentElement.scrollTop || document.body.scrollTop;
-            const header =  document.querySelector("header")
-            if (posicaoScroll === 0) {
-              header.style.background = '#7401DF'
-            } else {
-              header.style.background = '#7401DFa1'
-            }
-          })
-
     return(
-        <header className="w-screen h-12 bg-secundary-purple fixed top-0 z-50">
+        <header className="w-screen h-12 bg-[#7401DFa1] fixed top-0 z-50">
             <nav className="h-full max-w-[1464px] w-[80%] m-auto flex gap-4 items-center justify-end">
                 <Link href={"https://github.com/JoaoLeandroo"} target="blank" title="Github">
                     <FaGithub size={30} color="#fff"/>
